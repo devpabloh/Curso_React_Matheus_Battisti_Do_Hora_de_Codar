202ad75ee33798ca2365415733c762fcca74ce38
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-07 08:58:32 -0300

    atualização

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,20 @@
+//Components
+import FirstComponent from "./components/FirstComponents";
+
+
+// style - css
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </>
+  )
+}
+
+export default App
