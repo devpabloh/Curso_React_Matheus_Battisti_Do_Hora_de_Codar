c25338c5666ce40a74f89c4043cb266f2b2f8aea
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-07 08:15:10 -0300

    att

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello world React
+      <h1>Hello world React</h1>
+      <p>Começamos a estudar react</p>
     </div>
   );
 }
