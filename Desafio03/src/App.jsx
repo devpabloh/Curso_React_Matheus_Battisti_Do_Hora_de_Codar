3106fe8e62d566aac957f21dd360fbad23bf77b1
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-08 08:33:58 -0300

    atualizaçaõ

--- Desafio03/src/App.jsx
+++ Desafio03/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Elo from "./assets/Captura de tela 2024-05-25 161756.png"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,13 +12,13 @@ function App() {
       <h1>Seção 3</h1>
       {/* Como usar uma imagem que está na pasta public */}
       <div>
-        <img src="/Captura de tela 2024-05-30 201537.png" alt="" />
+        <img src="/Captura de tela 2024-05-30 201537.png" alt="campeão league of legends ornn" />
       </div>
 
       {/* como usar uma imagem que está dentro do Assets 
-      Você tem que importar ela como está na parte de cima da página, e depois você coloca na tag img, a tag div é opicional*/}
+      Você tem que importar ela como está na parte de cima da página, e depois você coloca na tag img, tirando as aspas duplas e colocando uma interpolação {e aqui dentro vai o nome que você deu na importação que criou}, a tag div é opicional*/}
       <div>
-        <img src="" alt="" />
+        <img src={Elo} alt="elo league of legends" />
       </div>
 
     </>
