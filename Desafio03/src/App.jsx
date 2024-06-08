093a43723d9a556272a6b39781d93483f703e0d5
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-08 08:21:17 -0300

    atualização

--- Desafio03/src/App.jsx
+++ Desafio03/src/App.jsx
@@ -3,12 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Seção 3</h1>
+      {/* Como usar uma imagem que está na pasta public */}
+      <div>
+        <img src="/Captura de tela 2024-05-30 201537.png" alt="" />
+      </div>
+
+      {/* como usar uma imagem que está dentro do Assets 
+      Você tem que importar ela como está na parte de cima da página, e depois você coloca na tag img, a tag div é opicional*/}
+      <div>
+        <img src="" alt="" />
+      </div>
+
     </>
   )
 }
