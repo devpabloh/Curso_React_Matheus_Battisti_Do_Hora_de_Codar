cc1626993957bc5f29def83c1c88c8e8ab7a042e
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-13 23:11:27 -0300

    Update ConditionalRender.js

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
@@ -2,12 +2,23 @@ import { useState } from "react";
 
 const ConditionalRender = () => {
     const[x] = useState(false);
-  return (
+    const [nome, setName] = useState("Tirulipa")
+    return (
     <div>
         <h1>Isso será exibido?</h1> 
         {x && <p>se x for true, sim!</p>}
+        {nome === "João" ? (
+            <div>
+                <p>O nome é João</p>
+            </div>
+        ) : (
+        <div>
+            <p>O nome não foi encontrado</p>
+        </div> 
+        )}
+        <button onClick={()=> setName("João")}>Click aqui para mudar o nome</button>
     </div>
-  )
+    )
 }
 
 export default ConditionalRender
