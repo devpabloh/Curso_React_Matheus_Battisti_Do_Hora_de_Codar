5bb0bb912f6abe77b1c69b7db9130ed773e543a1
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-13 23:56:52 -0300

    atualização

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -3,14 +3,21 @@ import './App.css';
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
+
+import {useState} from "react";
 
 function App() {
+  let name = "Tata"
+  const [userName] = useState("Maria");
   return (
   <>
+
   <p>Meu primeiro projeto</p>
   <ManageData/>
   <ListRender/>
   <ConditionalRender/>
+  <ShowUserName name={userName}/> 
   </>
   );
 }
