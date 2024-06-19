120c8271d3fb8915587bc220c313cb59eba1d93c
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-19 15:25:53 -0300

    atualização

--- 5_FORM_EM_REACT/form/src/Components/MyForm/MyForm.js
+++ 5_FORM_EM_REACT/form/src/Components/MyForm/MyForm.js
@@ -1,6 +1,19 @@
+import { useState } from "react"
 import "./MyForm.module.css"
 
 function MyForm() {
+
+    // Gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e)=>{
+        setName(e.target.value)
+    }
+
+    console.log(name)
+    console.log(email)
+
     return (
         <>
         
@@ -8,7 +21,7 @@ function MyForm() {
         <form >
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite seu nome" />
+                <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName} />
             </div>
             <input type="submit" value="Enviar" />
         </form>
@@ -16,7 +29,9 @@ function MyForm() {
 
         <label>
             <span>E-mail</span>
-            <input type="email" name="email" placeholder="Digite o seu e-mail" />
+            <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e)=>{
+                setEmail(e.target.value)
+            }} />
         </label>
         
         </>
