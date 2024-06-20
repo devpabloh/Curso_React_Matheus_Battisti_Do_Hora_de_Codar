f9024758d42bdeb0ac18c34e9ff96e6828d0b1ed
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-20 10:49:18 -0300

    Update MyForm.js

--- 5_FORM_EM_REACT/form/src/Components/MyForm/MyForm.js
+++ 5_FORM_EM_REACT/form/src/Components/MyForm/MyForm.js
@@ -3,18 +3,27 @@ import "./MyForm.module.css"
 
 const MyForm = ({user})=> {
 
-    // Gerenciamento de dados
+    // Gerenciamento de dados 
     const [name, setName] = useState(user ? user.name: "")
     const [email, setEmail] = useState(user ? user.email: "")
+    const [textArea, setTextArea] = useState(user ? user.textArea: "")
+    const [select, setSelect] = useState(user? user.select : "")
 
     const handleName = (e)=>{
         setName(e.target.value)
     }
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) =>{  
         event.preventDefault();
         console.log("Enviando formulário")
-        console.log(name, email)
+        console.log(name, email, textArea, select) //mostrando os dados que foram digitados no console
+
+        /* Limpar formulário */
+
+        setName("")
+        setEmail("")
+        setTextArea("")
+        setSelect("")
     }
 
     return (
@@ -28,16 +37,36 @@ const MyForm = ({user})=> {
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName} value={name}/>
             </div>
+
+               {/* 2 - Label envolvendo o input  */}
+
+            <label>
+                <span>E-mail</span>
+                {/* 4 - Simplificação de manipulação de state */}
+                <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e)=>{
+                    setEmail(e.target.value)}} value={email} className="meio"/>
+            </label>
+
+            <label>
+                <span>Bio:</span>
+                <textarea name="bio" placeholder="Fale sobre você" onChange={(e)=>{
+                    setTextArea(e.target.value)
+                }} value={textArea}></textarea>
+            </label>
+
+            <label>
+                <select name="role"  onChange={(e)=>{
+                    setSelect(e.target.value)
+                }} value={select}>
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="administrador">Administrador</option>
+                </select>
+            </label>
+
             <input type="submit" value="Enviar" />
         </form>
-        {/* 2 - Label envolvendo o input  */}
 
-        <label>
-            <span>E-mail</span>
-            {/* 4 - Simplificação de manipulação de state */}
-            <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e)=>{
-                setEmail(e.target.value)}} value={email} className="meio"/>
-        </label>
         
         </>
     )
