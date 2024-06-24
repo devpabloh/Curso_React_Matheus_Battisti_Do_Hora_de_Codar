7b51be02b1eb8f991dd015ad13a87afb468e1c8a
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-24 14:12:33 -0300

    atualização

--- 07_REQ_HTTP/httpreact/src/App.jsx
+++ 07_REQ_HTTP/httpreact/src/App.jsx
@@ -6,6 +6,8 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice]= useState("")
 
   // 1 - resgatando dados
   useEffect(() => {
@@ -17,19 +19,39 @@ function App() {
     fetchData();
   },[]);
 
-  console.log(products)
+  const handleSubmit = async (e)=>{
+
+  }
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
       <ul>
-        {products.map((product)=>(
-           <li key={product.id}>
-            {product.name} - R$: {product.price}
+        {products.map((produtos)=>(
+           <li key={produtos.id}>
+            {produtos.name} 
             </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name='name' onChange={(e)=>{
+              setName(e.target.value)
+            }} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name='price' onChange={(e)=>{
+              setPrice(e.target.value)
+            }} />
+            <input type="submit" value={"Criar produto"} className='add-product' />
+          </label>
+        </form>
+      </div>
     </div>
+
   );
 }
 
