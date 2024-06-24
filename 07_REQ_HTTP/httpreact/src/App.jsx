5280cb07333ee2c07e54c8b417cc30666b6d3e57
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-24 11:49:51 -0300

    atualização

--- 07_REQ_HTTP/httpreact/src/App.jsx
+++ 07_REQ_HTTP/httpreact/src/App.jsx
@@ -7,20 +7,28 @@ const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
 
+  // 1 - resgatando dados
   useEffect(() => {
-    async function fetchdata(){
+    async function fetchData(){
       const res = await fetch(url);
-      const data = res.json();
+      const data = await res.json();
       setProducts(data);
     }
-    fetchdata()
+    fetchData();
   },[]);
 
-  console.log(products);
+  console.log(products)
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product)=>(
+           <li key={product.id}>
+            {product.name} - {product.price}
+            </li>
+        ))}
+      </ul>
     </div>
   );
 }
