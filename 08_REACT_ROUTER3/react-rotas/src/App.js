70eb4377d7759257d8156809b86ce866881a5b61
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-01 15:10:04 -0300

    está funcionando ainda

--- 08_REACT_ROUTER3/react-rotas/src/App.js
+++ 08_REACT_ROUTER3/react-rotas/src/App.js
@@ -5,8 +5,9 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 // 2 - pages import
-import About from "./pages/About"
-import Home from "./pages/Home"
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 // 3 - Importar os componentes
 import NavBar from './components/NavBar/NavBar';
@@ -18,9 +19,11 @@ function App() {
       <BrowserRouter> {/* Os elementos fora do BrowserRouter serão exibidos em todas as páginas. */}
         <NavBar/>
         <Routes>
-          <Route path="/" element={<Home/>}></Route> {/* path (caminho), element é o componente que vai ser impresso quando o usuário acessar essa rota/caminho */}
+          <Route path="/" element={<Home/>}></Route> {/* path (caminho), element é o componente que vai ser impresso quando o usuário acessar essa rota/caminho */} 
 
           <Route path="/About" element={<About/>}></Route>
+          {/* 4 - Rota dinâmica */}
+          <Route path="/products/:id" element={<Product/>}></Route>
         </Routes>
       </BrowserRouter>
       
