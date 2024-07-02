9a4cdff7586af5aa8423d63f063c1f107f905eca
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-01 22:41:42 -0300

    0000

--- 08_REACT_ROUTER3/react-rotas/src/pages/Product.js
+++ 08_REACT_ROUTER3/react-rotas/src/pages/Product.js
@@ -1,6 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../Hook/useFetch";
+
 const Product = () => {
+    // 4 - Rota dinâmica
+    const {id} = useParams();
+
+    // 5 - Carregamento individual
+    const url = "http://localhost:3000/products/" + id;
+
+    const{data:product, loading, error} = useFetch(url)
+
+    console.log(product)
+
+
   return (
-    <div>product</div>
+    <>
+    
+    <p>ID do produto: {id}</p>
+    {error && <p>Ocorreu um erro...</p>}
+    {loading && <p>Carregando...</p>}
+    {product && (
+        <div>
+            <h1>{product.name}</h1>
+            <p>R$ {product.price}</p>
+        </div>
+    )}
+    
+    </>
   )
 }
 
