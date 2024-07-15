7ab9f3e3e0610b556295bea5049909c9149a8ba2
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-15 13:10:22 -0300

    atualizaçã

---
+++ 09_REACT_CONTEXT_API_3/context/src/context/CounterContext.js
@@ -0,0 +1,15 @@
+// 1 - criar context (contexto)
+import {createContext, useState } from "react";
+
+export const CounterContext = createContext();
+
+// 2 - criar provider
+
+export const CounterContextProvider = ({Children}) => {
+    const [counter, setCounter] = useState(5);
+    return (
+        <CounterContext.Provider value={{counter, setCounter}}>
+            {Children}
+        </CounterContext.Provider>
+    );
+};
