95a07eedae1f5d1443833cde94e456ab6a96c5c7
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-24 17:53:12 -0300

    Revert "atualização"
    This reverts commit e32727caa1e4ba691f9cd90dc01b30feeba33b9e.

--- 09_REACT_CONTEXT_API_2/context/src/pages/About.js
+++ 09_REACT_CONTEXT_API_2/context/src/pages/About.js
@@ -1,13 +1,8 @@
-import { useContext } from "react";
-import { CounterContext } from "../Context/CounterContext";
+
 
 const About = () => {
-  const {counter} = useContext(CounterContext)
   return (
-    <div>
-      <h1>About</h1>
-      <p>Valor doc contador: {counter}</p>
-    </div>
+    <div>About</div>
   )
 }
 
--- 09_REACT_CONTEXT_API_2/context/src/pages/Home.js
+++ 09_REACT_CONTEXT_API_2/context/src/pages/Home.js
@@ -1,6 +1,5 @@
 import { useContext } from "react"
 import {CounterContext} from "../Context/CounterContext"
-import ChangeCounter from "../Components/ChangeCounter";
 
 const Home = () => {
   const {counter} = useContext(CounterContext);
@@ -8,8 +7,6 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <p>Valor do contador: {counter}</p>
-      {/* 3 - Alterando valor com texto */}
-      <ChangeCounter/>
     </div>
   )
 }
